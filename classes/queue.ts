9a198c974ef2def459b12626137634cb5a2c446c
Tom Anderson <tom@ando.gq>
2020-06-08 14:49:38 +1000

    Make Queue ts

--- classes/queue.ts
+++ classes/queue.ts
@@ -1,22 +1,27 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Queue {
+export class Queue {
+    queue: object;
+
     constructor() {
         this.queue = {};
     }
-    add(item) {
+
+    add(item: any): string {
         let id;
+
         do {
             id = Math.floor(Math.random() * Math.pow(10, 6)).toString(16);
         } while (this.queue[id]);
+
         this.queue[id] = item;
+
         return id;
     }
-    get(id) {
+
+    get(id: string): any {
         return this.queue[id];
     }
-    exists(id) {
+
+    exists(id: string): boolean {
         return this.queue.hasOwnProperty(id);
     }
 }
-exports.Queue = Queue;
